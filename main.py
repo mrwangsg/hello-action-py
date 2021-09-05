d71b095a711b5cebb83ac5847180903493f446af
sgwang <911193849@qq.com>
2021-09-05 15:23:08 +0800

    提交

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('action with python')
+    print_hi('action with python')    
 
     # 忽略警告
     requests.packages.urllib3.disable_warnings()
